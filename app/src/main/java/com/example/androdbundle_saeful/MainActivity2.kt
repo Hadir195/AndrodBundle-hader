465809777640e89aa2e98d8a0194306b2f353498
Msaeful <kiyotakaa787@gmail.com>
2024-01-15 11:42:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androdbundle_saeful/MainActivity2.kt
@@ -0,0 +1,22 @@
+package com.example.androdbundle_saeful
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var namaView: TextView
+    private lateinit var umurView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        namaView = findViewById(R.id.namaView)
+        umurView = findViewById(R.id.umurView)
+        val extras = intent.extras
+        namaView.text = extras?.getString("nama")
+        umurView.text = extras?.getString("umur")
+    }
+}
